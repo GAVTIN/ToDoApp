ffb7764500d0c9649977e1673a80f5e5864446b0
Gaurav Kumar Sinha <gauravkumarsinhasinha@gmail.com>
2022-01-17 00:32:13 +0530

    my first commit for to do app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Board from './components/Board'
+import Card from './components/Card'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>Drag Drop To Do List</h2>
+      <main className="flexbox">
+        <Board id="board-1" className="board">
+        <h1>To Be Done List</h1>
+            <Card id="card-1" className="card" draggable="true">
+                <p>NodeJS</p>
+            </Card>
+            <Card id="card-3" className="card" draggable="true">
+                <p>API Services</p>
+            </Card>
+            <Card id="card-4" className="card" draggable="true">
+                <p>MongoDB</p>
+            </Card>
+            <Card id="card-5" className="card" draggable="true">
+                <p>Redux</p>
+            </Card>
+        </Board>
+        
+        <Board id="board-2" className="board">
+        <h1>Done List</h1>
+            <Card id="card-2" className="card" draggable="true">
+                <p>HTML/CSS</p>
+            </Card>
+            <Card id="card-7" className="card" draggable="true">
+                <p>DOM Event Handling</p>
+            </Card>
+            <Card id="card-8" className="card" draggable="true">
+                <p>JAVASCRIPT</p>
+            </Card>
+            <Card id="card-9" className="card" draggable="true">
+                <p>Event Loop/Hooks</p>
+            </Card>
+        </Board>
+
+      </main>
     </div>
   );
 }
